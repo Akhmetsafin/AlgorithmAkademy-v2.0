c8e976f9f6552c7b81d04f92e706b917853615f4
Artur <artur071982@gmail.com>
2021-12-13 15:36:25 +0500

    MainMenu Created

--- MainMenu.cs
+++ MainMenu.cs
@@ -8,5 +8,40 @@ namespace AlgorithmAkademy_v2._0
 {
     class MainMenu
     {
+        /// <summary>
+        /// Главное Меню для программы
+        /// </summary>
+        /// <returns></returns>
+        public void MyMenu()
+        {
+            string text = Console.ReadLine();
+            do
+            {
+                if (int.TryParse(text, out int numberInter))
+                {
+                    
+                    if (numberInter == 1)
+                       // SimpleNumbers();
+
+                    if (numberInter == 2)
+                       // FibonachiNumbers();
+
+                    if (numberInter == 3)
+                      //  DualLinkedList();
+
+                    if (numberInter == 9)
+                        Environment.Exit(0);
+                    Console.WriteLine("Для выхода в главное меню нажмите ESC");
+                }
+                else
+                {
+                    Console.WriteLine("Команда не распознана");
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            Console.Clear();
+            Console.WriteLine(menuText);
+            MyMenu();
+        }
     }
 }
