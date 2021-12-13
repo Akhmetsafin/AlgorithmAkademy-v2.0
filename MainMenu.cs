d1850e35066d8ec721893350006e2f48e1028268
Artur <artur071982@gmail.com>
2021-12-13 15:52:05 +0500

    Creat Branch

--- MainMenu.cs
+++ MainMenu.cs
@@ -8,6 +8,12 @@ namespace AlgorithmAkademy_v2._0
 {
     class MainMenu
     {
+        public string menuText = "Добро пожаловать в меню Домашних заданий по предмету Алгоритмы \n" +
+                 "Выберите ниже из списка:\n" +
+                 "1-Урок №1- Проверка числа простое/непростое\n" +
+                 "2-Урок №1- Число Фибоначи рекурсивный метотод / обычный метод\n" +
+                 "3-Урок №2- Двусвязный список\n" +
+                 "9-Выход";
         /// <summary>
         /// Главное Меню для программы
         /// </summary>
@@ -19,7 +25,6 @@ namespace AlgorithmAkademy_v2._0
             {
                 if (int.TryParse(text, out int numberInter))
                 {
-                    
                     if (numberInter == 1)
                        // SimpleNumbers();
 
